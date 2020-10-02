03091a654e24644a361e36a4a33a0af087d712de
ankynator <ankynator@gmail.com>
2020-10-01 20:29:50 -0500

    Ajustando a las distintas pantallas

--- src/index.js
+++ src/index.js
@@ -5,30 +5,12 @@ const leftData = document.getElementById("Left__data");
 storageRange.oninput = function () {
   useData.innerHTML = this.value;
   leftData.innerHTML = 1000 - this.value;
-
-  let val = calcPos(this.value);
   let color =
     "linear-gradient(90deg, rgba(237,159,114,1) 0%,  rgba(237,67,174,1)" +
-    val +
+    this.value / 10 +
     "%, rgb(12, 18, 44)" +
-    val +
+    this.value / 10 +
     "%)";
 
   storageRange.style.background = color;
 };
-
-//ajuste de la posicion para pintar correctamente el progress bar
-function calcPos(val) {
-  val = val / 10;
-  console.log(val);
-
-  if (val < 30) {
-    val = val + 2;
-  }
-
-  if (val > 60) {
-    val = val - 2;
-  }
-
-  return val;
-}
